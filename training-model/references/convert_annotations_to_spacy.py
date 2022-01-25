46b4c0ec18b4bc0868687ae3006c7e6e490bdd67
Vitaly Davidenko <vdaviden@wiley.com>
2022-01-25 22:12:35 +0300

    ProcessPoolExecutor for processing in parts

--- training-model/references/convert_annotations_to_spacy.py
+++ training-model/references/convert_annotations_to_spacy.py
@@ -1,12 +1,14 @@
+from concurrent.futures import Future, ProcessPoolExecutor
 import logging
 from pathlib import Path
 from typing import List, Optional
-
+from tqdm import tqdm
 from lxml import etree
 from numpy.random import randint
 import spacy
 from spacy.tokens import Doc, DocBin
 import typer
+import numpy
 
 from csl_client import make_bibliography, styles_list
 from lxml_iter_tree import annotations
@@ -42,9 +44,8 @@ def references_to_spacy(references, style: str) -> Optional[Doc]:
         if span.label_ == tag_sentence_start:
             span[0].is_sent_start = True
 
-    from pprint import pprint
-
-    pprint([(span.label_, span.text) for span in spans if span])
+    # from pprint import pprint
+    # pprint([(span.label_, span.text) for span in spans if span])
 
     return doc
 
@@ -54,27 +55,54 @@ def split_up_large_doc(doc: Doc, max_tokens=512) -> List[Doc]:
     return [doc]
 
 
-def main(crossref_dir: Path):
+def convert(
+    crossref_files: List[Path], docbin_path: Path = Path("bibliographies.docbin")
+):
     styles: List[str] = styles_list()
     len_styles = len(styles)
     print(f"CSL processor supports {len_styles} styles")
 
-    for f in list(crossref_dir.glob("**/*.json")):
-        bibliographies = make_bibliography(
-            f, randint(0, len_styles, NUM_STYLES_FOR_DOC).tolist()
-        )
-        for bibliography in bibliographies:
-            if "references" not in bibliography:
-                print("a problem for style ", bibliography["style"])
-                continue
-            references = bibliography["references"]
-            style = bibliography["style"]
-            doc = references_to_spacy(references, style)
-            if doc:
-                docs = split_up_large_doc(doc)
-                # TODO save as DocBin
-            else:
-                log.warning("Can't process %s", f)
+    db = DocBin(store_user_data=True)
+    try:
+        for f in tqdm(crossref_files, desc=docbin_path.name):
+            bibliographies = make_bibliography(
+                f, randint(0, len_styles, NUM_STYLES_FOR_DOC).tolist()
+            )
+            for bibliography in bibliographies:
+                if "references" not in bibliography:
+                    print("a problem for style ", bibliography["style"])
+                    continue
+                references = bibliography["references"]
+                style = bibliography["style"]
+                doc = references_to_spacy(references, style)
+                if doc:
+                    docs = split_up_large_doc(doc)
+                    for d in docs:
+                        db.add(d)
+                else:
+                    log.warning("Can't process %s", f)
+    finally:
+        db.to_disk(docbin_path)
+        return docbin_path
+
+
+def main(
+    crossref_dir: Path, output_dir: Path = Path(), parts: int = 100, parallel: int = 2
+):
+    input_files = [path for path in crossref_dir.glob("**/*.json")]
+    input_files_parts = [
+        arr.tolist() for arr in numpy.array_split(numpy.array(input_files), parts)
+    ]
+    futures: List[Future] = []
+    with ProcessPoolExecutor(parallel) as e:
+        for i, input_files_part in enumerate(input_files_parts):
+            futures.append(
+                e.submit(
+                    convert, input_files_part, output_dir / f"references.{i}.docbin"
+                )
+            )
+        for f in futures:
+            print("convert:done:", f.result())
 
 
 if __name__ == "__main__":
