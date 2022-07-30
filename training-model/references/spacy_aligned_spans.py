e190422983e3954d6b70834d110502be818d1468
Vitalik Davidenko <vitaly.dvd@gmail.com>
2022-07-30 16:38:02 +0300

    \t added to get_aligned spans

--- training-model/references/spacy_aligned_spans.py
+++ training-model/references/spacy_aligned_spans.py
@@ -2,6 +2,10 @@ from spacy.tokens import Span
 
 # https://github.com/explosion/spaCy/blob/2d89dd9db898e66058bf965e1b483b0019ce1b35/spacy/training/example.pyx#L296
 # that has been modified to support '\n' within a span
+
+_space_translation = str.maketrans({" ": "", "\n": "", "\t": ""})
+
+
 def _get_aligned_spans(doc, spans, align, allow_overlap):
     seen = set()
     output = []
@@ -11,10 +15,8 @@ def _get_aligned_spans(doc, spans, align, allow_overlap):
             indices = [idx for idx in indices if idx not in seen]
         if len(indices) >= 1:
             aligned_span = Span(doc, indices[0], indices[-1] + 1, label=span.label)
-            target_text = span.text.lower().strip().replace(" ", "").replace("\n", "")
-            our_text = (
-                aligned_span.text.lower().strip().replace(" ", "").replace("\n", "")
-            )
+            target_text = span.text.lower().strip().translate(_space_translation)
+            our_text = aligned_span.text.lower().strip().translate(_space_translation)
             if our_text == target_text:
                 output.append(aligned_span)
                 seen.update(indices)
